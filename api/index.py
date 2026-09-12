03223627b81a1bb88305713db410ea2887cc83f4
JITESH-KUMAR05 <jitesh.kumar05official@gmail.com>
2026-09-13 00:04:09 +0530

    added the about page

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add parent directory to sys.path so modules (geometry_engine, cad_exporter, app) resolve correctly
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from app import app
